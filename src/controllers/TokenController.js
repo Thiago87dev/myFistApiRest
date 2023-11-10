d42b2e5f693b48358d0f1e2b9446cac0f47cebfc
thiago alves <thiago87gsa@gmail.com>
2023-11-10 08:34:40 -0300

    creating token

---
+++ src/controllers/TokenController.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/User'
+
+class TokenController {
+  async store(req, res) {
+    const { email = '', password = '' } = req.body
+
+    if (!email || !password) {
+      return res.status(401).json({
+        errors: ['Email e senha devem ser enviados.'],
+      })
+    }
+
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      return res.status(401).json({
+        errors: ['Usuário não existe.'],
+      })
+    }
+
+    if (!(await user.passwordIsValid(password))) {
+      return res.status(401).json({
+        errors: ['Senha Inválida.'],
+      })
+    }
+
+    const { id } = user
+    const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXPIRATION,
+    })
+
+    return res.json({ token })
+  }
+}
+
+export default new TokenController()
